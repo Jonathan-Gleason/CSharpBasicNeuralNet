40408c30a752fe5fb3c624a780652bb788e3ff79
Jonathan-Gleason <91348968+Jonathan-Gleason@users.noreply.github.com>
2022-04-29 16:46:51 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,255 @@
+﻿using System;
+using System.Windows;
+using Accord.Math;
+
+namespace NeuralNet
+{
+    class Program
+    {
+
+        static void PrintMatrix(double[,] matrix)
+        {
+            int rowLength = matrix.GetLength(0);
+            int colLength = matrix.GetLength(1);
+
+            for (int i = 0; i < rowLength; i++)
+            {
+                for (int j = 0; j < colLength; j++)
+                {
+                    Console.Write(string.Format("{0} ", matrix[i, j]));
+                }
+                Console.Write(Environment.NewLine);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var curNeuralNetwork = new NeuralNetwork(1, 3);
+
+            Console.WriteLine("Synaptic weights before training:");
+            PrintMatrix(curNeuralNetwork.SynapseMatrix);
+
+            var trainingInputs = new double[,] { { 0, 0, 1 }, { 1, 1, 1 }, { 1, 0, 1 }, { 0, 1, 1 } };
+            var trainingOutputs = NeuralNetwork.MatrixTranspose(new double[,] { { 0, 1, 0, 1 } });
+
+            curNeuralNetwork.Train(trainingInputs, trainingOutputs, 10000);
+
+            Console.WriteLine("\nSynaptic weights after training:");
+            PrintMatrix(curNeuralNetwork.SynapseMatrix);
+
+
+            // testing neural networks against a new problem 
+            var output = curNeuralNetwork.Think(new double[,] { {0,1,0 } });
+            //var output = curNeuralNetwork.Think(new double[,] { { 326,12,0,0,0,0,1 } });
+            Console.WriteLine("\nConsidering new problem [0, 1, 0] => :");
+            PrintMatrix(output);
+        }
+    }
+    class NeuralNetwork
+    {
+        private Random _randomObj;
+        public NeuralNetwork(int synapseMatrixColumns, int synapseMatrixLines)
+        {
+            SynapseMatrixColumns = synapseMatrixColumns;
+            SynapseMatrixLines = synapseMatrixLines;
+
+            _Init();
+        }
+
+        public int SynapseMatrixColumns { get; }
+        public int SynapseMatrixLines { get; }
+        public double[,] SynapseMatrix { get; private set; }
+
+        private void _Init()
+        {
+            _randomObj = new Random(1);
+            _GenerateSynapsesMatrix();
+        }
+
+        private void _GenerateSynapsesMatrix()
+        {
+            SynapseMatrix = new double[SynapseMatrixLines, SynapseMatrixColumns];
+            for(var i = 0; i < SynapseMatrixLines; i++)
+            {
+                for(var j = 0; j < SynapseMatrixColumns; j++)
+                {
+                    SynapseMatrix[i, j] = (2 * _randomObj.NextDouble()) - 1;
+                }
+            }
+        }
+
+        private double[,] _CalculateSigmoid(double[,] matrix)
+        {
+            int rowLength = matrix.GetLength(0);
+            int colLength = matrix.GetLength(1);
+
+            for (int i = 0; i < rowLength; i++)
+            {
+                for (int j = 0; j < colLength; j++)
+                {
+                    var value = matrix[i, j];
+                    matrix[i, j] = 1 / (1 + Math.Exp(value * -1));
+                }
+            }
+
+            return matrix;
+        }
+
+        private double[,] _CalculateSigmoidDerivative(double[,] matrix)
+        {
+            int rowLength = matrix.GetLength(0);
+            int colLength = matrix.GetLength(1);
+
+            for (int i = 0; i < rowLength; i++)
+            {
+                for (int j = 0; j < colLength; j++)
+                {
+                    var value = matrix[i, j];
+                    matrix[i, j] = value * (1 - value);
+                }
+            }
+
+            return matrix;
+        }
+
+        public double[,] Think(double[,] inputMatrix)
+        {
+            var productOfTheInputsAndWeights = MatrixDotProduct(inputMatrix, SynapseMatrix);
+
+            return _CalculateSigmoid(productOfTheInputsAndWeights);
+        }
+
+        public void Train(double[,] trainInputMatrix, double[,] trainOutputMatrix, int interactions)
+        {
+            for(var i = 0; i < interactions; i++)
+            {
+                var output = Think(trainInputMatrix);
+
+                var error = MatrixSubstract(trainOutputMatrix, output);
+                var curSigmoidDerivative = _CalculateSigmoidDerivative(output);
+                var error_SigmoidDerivative = MatrixProduct(error, curSigmoidDerivative);
+
+                var adjustment = MatrixDotProduct(MatrixTranspose(trainInputMatrix), error_SigmoidDerivative);
+
+                SynapseMatrix = MatrixSum(SynapseMatrix, adjustment);
+            }
+        }
+        /// <summary>
+        /// Transpose a matrix
+        /// </summary>
+        /// <returns></returns>
+        public static double[,] MatrixTranspose(double[,] matrix)
+        {
+            int w = matrix.GetLength(0);
+            int h = matrix.GetLength(1);
+
+            double[,] result = new double[h, w];
+
+            for (int i = 0; i < w; i++)
+            {
+                for (int j = 0; j < h; j++)
+                {
+                    result[j, i] = matrix[i, j];
+                }
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Sum one matrix with another
+        /// </summary>
+        /// <returns></returns>
+        public static double[,] MatrixSum(double[,] matrixa, double[,] matrixb)
+        {
+            var rowsA = matrixa.GetLength(0);
+            var colsA = matrixa.GetLength(1);
+
+            var result = new double[rowsA, colsA];
+
+            for (int i = 0; i < rowsA; i++)
+            {
+                for (int u = 0; u < colsA; u++)
+                {
+                    result[i, u] = matrixa[i, u] + matrixb[i, u];
+                }
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Subtract one matrix from another
+        /// </summary>
+        /// <returns></returns>
+        public static double[,] MatrixSubstract(double[,] matrixa, double[,] matrixb)
+        {
+            var rowsA = matrixa.GetLength(0);
+            var colsA = matrixa.GetLength(1);
+
+            var result = new double[rowsA, colsA];
+
+            for (int i = 0; i < rowsA; i++)
+            {
+                for (int u = 0; u < colsA; u++)
+                {
+                    result[i, u] = matrixa[i, u] - matrixb[i, u];
+                }
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Multiplication of a matrix
+        /// </summary>
+        /// <returns></returns>
+        public static double[,] MatrixProduct(double[,] matrixa, double[,] matrixb)
+        {
+            var rowsA = matrixa.GetLength(0);
+            var colsA = matrixa.GetLength(1);
+
+            var result = new double[rowsA, colsA];
+
+            for (int i = 0; i < rowsA; i++)
+            {
+                for (int u = 0; u < colsA; u++)
+                {
+                    result[i, u] = matrixa[i, u] * matrixb[i, u];
+                }
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Dot Multiplication of a matrix
+        /// </summary>
+        /// <returns></returns>
+        public static double[,] MatrixDotProduct(double[,] matrixa, double[,] matrixb)
+        {
+
+            var rowsA = matrixa.GetLength(0);
+            var colsA = matrixa.GetLength(1);
+
+            var rowsB = matrixb.GetLength(0);
+            var colsB = matrixb.GetLength(1);
+
+            if (colsA != rowsB)
+                throw new Exception("Matrices dimensions don't fit.");
+
+            var result = new double[rowsA, colsB];
+
+            for (int i = 0; i < rowsA; i++)
+            {
+                for (int j = 0; j < colsB; j++)
+                {
+                    for (int k = 0; k < rowsB; k++)
+                        result[i, j] += matrixa[i, k] * matrixb[k, j];
+                }
+            }
+            return result;
+        }
+
+    }
+}
